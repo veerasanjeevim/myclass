1d4f3f73e4a6b2b82ef95b956ade82d28635166f
veerasanjeevim <veerasanjeevim@gmail.com>
2026-06-07 19:51:41 +0530

    shashja

---
+++ javascript/var.js
@@ -0,0 +1,175 @@
+
+var play;
+console.log(play)
+var move;
+console.log(move)
+var start;
+console.log(start)
+var gbu;
+console.log(gbu)
+var monitor;
+console.log(monitor)
+var sound;
+console.log(sound)
+var has;
+console.log(has)
+var joy;
+console.log(joy)
+var moving;
+console.log(moving)
+var startup;
+console.log(startup)
+var enjoy;
+console.log(enjoy)
+var happy;
+console.log(happy)
+var sad;
+console.log(sad)
+var whale;
+console.log(whale)
+var happy;
+console.log(happy)
+var sentiment;
+console.log(sentiment)
+var chance;
+console.log(chance)
+var tube;
+console.log(tube)
+var youtube;
+console.log(youtube)
+var fun;
+console.log(fun)
+var party;
+console.log(party)
+var long;
+console.log(long)
+var sun;
+console.log(sun)
+var planet;
+console.log(planet)
+var fuller;
+console.log(fuller)
+var waymore;
+console.log(waymore)
+var hangout;
+console.log(hangout)
+var moveless;
+console.log(moveles)
+var sad;
+console.log(sad)
+var laughing;
+console.log(laughing)
+let sing;
+ console.log(sing)
+let walk;
+ console.log(walk)
+let jump;
+ console.log(jump)
+ let run;
+ console.log(run)
+ let scam;
+ console.log(scam)
+ let fast;
+ console.log(fast)
+ let container;
+ console.log(container)
+ let happy;
+ console.log(happy)
+ let scare;
+ console.log(scare)
+ let hymp;
+ console.log(hymp)
+ let runfats;
+ console.log(runfast)
+ let doit;
+ console.log(doit)
+ let move;
+ console.log(move)
+ let walk;
+ console.log(walk)
+ let swim;
+ console.log(swim)
+ let speak;
+ console.log(speak)
+ let sung;
+ console.log(sung)
+ let groom;
+ console.log(groom)
+ let hump;
+ console.log(hump)
+ let skip;
+ console.log(skip)
+ let sped;
+ console.log(sped)
+ let work;
+ console.log(work)
+ let dofast;
+ console.log(dofast)
+ let rule;
+ console.log(rule)
+ let worker;
+ console.log(worker)
+ let kingdom;
+ console.log(kingdom)
+ let princess;
+ console.log(princess)
+ let onspot;
+ console.log(onspot)
+ let groom;
+ console.log(groom)
+const tiger;
+console.log(tiger)
+const mike;
+console.log(mike)
+const number;
+console.log(number)
+const color;
+console.log(color)
+const horse;
+console.log(horse)
+const hump;
+console.log(hump)
+const push;
+console.log(push)
+const fileexplorer;
+console.log(fileexplorer)
+const pushandpull;
+console.log(pushandpull)
+const ground;
+console.log(ground)
+const hot;
+console.log(hot)
+const waymore;
+console.log(waymore)
+const colourbox;
+console.log(colorbox)
+const harash;
+console.log(harash)
+const moveaway;
+console.log(moveaway)
+const runfast;
+console.log(runfast)
+const googleit;
+console.log(googleit)
+const horeseride;
+console.log(horeseride)
+const neterprenur;
+console.log(neterprenur)
+const hiibro;
+console.log(hiibro)
+const runaway;
+console.log(runaway)
+const highways;
+console.log(highways)
+const happy;
+console.log( happy)
+const sadnews;
+console.log(sadnews)
+const driving;
+console.log(driving)
+const hiking;
+console.log( hiking)
+const goingway;
+console.log(goingway)
+const highon;
+console.log(highon)
